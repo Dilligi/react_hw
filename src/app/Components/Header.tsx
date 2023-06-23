e458b8b6f5e959a1220a9967a7f4eb1be6cf05a1
Dilligi <as6053825@gmail.com>
2023-06-23 16:19:30 +0300

    Some styles is made

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
-import styles from '../styles/header.module.css'
+import mainStyles from '../styles/main.module.css'
+import HeaderStyles from '../styles/header.module.css'
 
 export default function Header() {
   return (
-    <header>
-        <h1 className={styles.header_title}>
-          Билетопоиск
-        </h1>
+    <header className={HeaderStyles.header}>
+        <div className={mainStyles.container}>
+          <h1 className={HeaderStyles.header_title}>
+            Билетопоиск
+          </h1>
+          <div>
+
+          </div>
+        </div>
     </header>
   )
 }
